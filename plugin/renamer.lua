13d3557cbe99745b6d018d2aba987b9e01796a5a
Marces Engel <git@marces.xyz>
2025-08-12 17:25:32 +0200

    chore: require manually invoking setup

--- plugin/renamer.lua
+++
@@ -1,5 +0,0 @@
--- Auto-init with defaults so commands exist even without explicit setup().
--- Users can still call require("renamer").setup({ ... }) later to override.
-pcall(function()
-  require("renamer").setup()
-end)
